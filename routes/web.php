b7cf742e116ac608fa6753cee833d6d7b96de605
Haydar Hilmy <88047909+haydar-hilmy@users.noreply.github.com>
2026-01-24 22:39:23 +0700

    Modul 12: Homepage Pembeli

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,14 @@ use App\Http\Controllers\Admin\EventController;
 use App\Http\Controllers\Admin\HistoriesController;
 use App\Http\Controllers\Admin\TiketController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-
-
 
 Route::middleware('auth')->group(function () {
     Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
